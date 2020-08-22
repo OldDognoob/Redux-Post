353a294b081658cd23bb97c2fe55e8e925724544
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-08-22 21:04:27 +0300

    wip

--- my-post/src/components/App.js
+++ my-post/src/components/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Lists from "./sub/index";
+import AddItem from "./sub/add";
+import EditItem from "./sub/edit";
+import "../styles.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Link, Route, BrowserRouter } from "react-router-dom";
+
+class App extends React.Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <nav>
+            <div className="nav-wrapper blue lighten-3">
+              <Link to="/" className="brand-logo right title">
+                Post
+              </Link>
+              <ul className="left">
+                <li>
+                  <Link to="/">
+                    <button className="btn purple">
+                      View List
+                    </button>
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/add">
+                    <button className="btn">Add Item</button>
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </nav>
+          <br />
+          <br />
+          <Route path="/" exact component={Lists} />
+          <Route path="/add" component={AddItem} />
+          <Route path="/edit/:id" component={EditItem} />
+          <br />
+          <br />
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
