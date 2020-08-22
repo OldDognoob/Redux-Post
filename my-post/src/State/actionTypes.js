35f757163d50c2f8682d388a8cf1e9f4b0f70f9a
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-08-22 21:34:14 +0300

    working project

---
+++ my-post/src/State/actionTypes.js
@@ -0,0 +1,20 @@
+// step 2: Design the types of Action that the State might suffer
+// Add item
+export const ADD_ITEM_START = 'ADD_ITEM_START'
+export const ADD_ITEM_SUCCESS = 'ADD_ITEM_SUCCESS'
+export const ADD_ITEM_FAILURE = 'ADD_ITEM_FAILURE'
+
+// Delete item
+export const DELETE_ITEM_START = 'DELETE_ITEM_START'
+export const DELETE_ITEM_SUCCESS = 'DELETE_ITEM_SUCCESS'
+export const DELETE_ITEM_FAILURE = 'DELETE_ITEM_FAILURE'
+
+// Edit item
+export const EDIT_ITEM_START = 'EDIT_ITEM_START'
+export const EDIT_ITEM_SUCCESS = 'EDIT_ITEM_SUCCESS'
+export const EDIT_ITEM_FAILURE = 'EDIT_ITEM_FAILURE'
+
+//Update item
+export const UPDATE_ITEM_START = 'UPDATE_ITEM_START'
+export const UPDATE_ITEM_SUCCESS = 'UPDATE_ITEM_SUCCESS'
+export const UPDATE_ITEM_FAILURE = 'UPDATE_ITEM_FAILURE'
